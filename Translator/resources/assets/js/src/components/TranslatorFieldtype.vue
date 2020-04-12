9fa46802b016daf69efad11859c563ffbe53e349
Michael Aerni <hello@michaelaerni.ch>
2020-04-12 16:59:35 +0200

    Add compilation for Vue components

---
+++ Translator/resources/assets/js/src/components/TranslatorFieldtype.vue
@@ -0,0 +1,28 @@
+<script>
+import axios from "axios";
+
+export default {
+  mixins: [Fieldtype],
+
+  template: '<button @click="translate" class="btn">Translate</button>',
+
+  computed: {
+    uri() {
+      return this.getUri();
+    }
+  },
+
+  methods: {
+    getUri: function() {
+      const path = window.location.pathname.split("/").slice(4);
+      const query = window.location.search;
+      return path + query;
+    },
+    translate: function() {
+      axios.get(`/cp/addons/translator/translate/${this.uri}`).then(() => {
+        location.reload();
+      });
+    }
+  }
+};
+</script>
---
+++ Translator/resources/assets/js/src/fieldtype.js
@@ -0,0 +1,3 @@
+import TranslatorFieldtype from "./components/TranslatorFieldtype.vue";
+
+Vue.component("translator-fieldtype", TranslatorFieldtype);
