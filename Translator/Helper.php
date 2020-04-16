245d8b027921128e484fd106c9820f45642cd16d
Michael Aerni <hello@michaelaerni.ch>
2020-04-16 12:13:27 +0200

    Add class with helper functions

---
+++ Translator/Helper.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Statamic\Addons\Translator;
+
+class Helper
+{
+    static public function array_map_recursive($callback, $input) {
+        $output = [];
+        foreach ($input as $key => $data) {
+            if (is_array($data)) {
+                $output[$key] = Self::array_map_recursive($callback, $data);
+            } else {
+                $output[$key] = $callback($data, $key);
+            }
+        }
+        return $output;
+    }
+
+    static public function multi_array_key_exists($key, array $array): bool
+    {
+        if (array_key_exists($key, $array)) {
+            return true;
+        } else {
+            foreach ($array as $nested) {
+                if (is_array($nested) && Self::multi_array_key_exists($key, $nested))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    static public function array_intersect_key_recursive(array $array1, array $array2) 
+    {
+        $array1 = array_intersect_key($array1, $array2);
+        
+        foreach ($array1 as $key => &$value) 
+        {
+            if (is_array($value)) 
+            {
+                $value = is_array($array2[$key]) ? Self::array_intersect_key_recursive($value, $array2[$key]) : $value;
+            }
+        }
+        return $array1;
+    }
+
+    static public function array_keys_recursive(array $array)
+    {
+        $keys = array();
+
+        foreach ($array as $key => $value) {
+            $keys[] = $key;
+
+            if (is_array($array[$key])) {
+                $keys = array_merge($keys, Self::array_keys_recursive($array[$key]));
+            }
+        }
+
+        return $keys;
+    }
+
+    static public function array_filter_recursive( array $array, callable $callback = null ) {
+        $array = is_callable( $callback ) ? array_filter( $array, $callback ) : array_filter( $array );
+        foreach ( $array as &$value ) {
+            if ( is_array( $value ) ) {
+                $value = call_user_func( __FUNCTION__, $value, $callback );
+            }
+        }
+    
+        return $array;
+    }
+}
