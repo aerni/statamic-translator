aba5b3e24685e6f2097cc854303df773c6753ec5
Michael Aerni <hello@michaelaerni.ch>
2020-04-26 17:23:56 +0200

    Rework controller methods for post requests

--- Translator/TranslatorController.php
+++ Translator/TranslatorController.php
@@ -2,28 +2,58 @@
 
 namespace Statamic\Addons\Translator;
 
+use Illuminate\Http\Request;
 use Statamic\Extend\Controller;
-use Statamic\API\Request;
 
 class TranslatorController extends Controller
 {
+    protected $googleTranslate;
     protected $translator;
 
-    public function __construct(Translator $translator)
+    public function __construct(GoogleTranslate $googleTranslate, Translator $translator)
     {
         parent::__construct();
 
+        $this->googleTranslate = $googleTranslate;
         $this->translator = $translator;
     }
-    
-    public function index(string $uri): void
-    {
-        $locale = Request::input('locale');
 
-        if (empty($locale)) {
-            return;
+    public function postSupportedLanguages(): array
+    {
+        return $this->googleTranslate->supportedLanguages();
+    }
+
+    public function postTranslate(Request $request): array
+    {
+        $id = $request->id;
+        $sourceLocale = $request->sourceLocale;
+        $targetLocale = $request->targetLocale;
+
+        if ($sourceLocale === $targetLocale) {
+            return [
+                'message' => 'Can not translate the default locale.',
+            ];
         }
 
-        $this->translator->translate($uri, $locale);
+        $this->translator->translate($id, $targetLocale);
+
+        return [
+            'message' => 'Translation successful!',
+        ];
+    }
+
+    public function getTranslate(string $id, string $sourceLocale, string $targetLocale): array
+    {
+        if ($sourceLocale === $targetLocale) {
+            return [
+                'message' => 'Can not translate the default locale.',
+            ];
+        }
+
+        $this->translator->translate($id, $targetLocale);
+
+        return [
+            'message' => 'Translation successful!',
+        ];
     }
 }
