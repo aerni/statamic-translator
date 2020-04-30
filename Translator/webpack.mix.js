341b88f44b29f5ce6ed86a70c02329bfa3fa5be7
Michael Aerni <hello@michaelaerni.ch>
2020-04-30 11:15:20 +0200

    Add tailwindcss

--- Translator/webpack.mix.js
+++ Translator/webpack.mix.js
@@ -1,3 +1,21 @@
-const mix = require("laravel-mix");
+const mix = require('laravel-mix');
+const path = require('path');
+require('laravel-mix-purgecss');
 
-mix.js("resources/assets/js/src/fieldtype.js", "resources/assets/js/");
+mix.setPublicPath('resources/assets/')
+    .postCss('resources/assets/css/src/translator.css', 'css', [
+        require('tailwindcss'),
+    ])
+    .js('resources/assets/js/src/fieldtype.js', 'js');
+
+if (mix.inProduction()) {
+    mix.purgeCss({
+        enabled: true,
+        content: [
+            path.join(__dirname, 'resources/assets/**/*.vue'),
+        ],
+        extensions: ['html', 'js', 'php', 'vue', 'svg', 'css', 'scss'],
+        extractorPattern: /[\w-/.:]+(?<!:)/g
+    })
+    .version();
+}
