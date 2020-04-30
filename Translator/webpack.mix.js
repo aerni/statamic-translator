d8b4c52b0acc58904cbcf523909b48e27c1a95a6
Michael Aerni <hello@michaelaerni.ch>
2020-04-30 17:58:50 +0200

    Move assets to new src folder

--- Translator/webpack.mix.js
+++ Translator/webpack.mix.js
@@ -3,16 +3,16 @@ const path = require('path');
 require('laravel-mix-purgecss');
 
 mix.setPublicPath('resources/assets/')
-    .postCss('resources/assets/css/src/translator.css', 'css', [
+    .postCss('src/resources/assets/css/translator.css', 'css', [
         require('tailwindcss'),
     ])
-    .js('resources/assets/js/src/fieldtype.js', 'js');
+    .js('src/resources/assets/js/fieldtype.js', 'js');
 
 if (mix.inProduction()) {
     mix.purgeCss({
         enabled: true,
         content: [
-            path.join(__dirname, 'resources/assets/**/*.vue'),
+            path.join(__dirname, 'src/resources/assets/**/*.vue'),
         ],
         extensions: ['html', 'js', 'php', 'vue', 'svg', 'css', 'scss'],
         extractorPattern: /[\w-/.:]+(?<!:)/g
