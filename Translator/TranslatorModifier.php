8828917025ede6a9cf1aff4b774b6885bc02349b
Michael Aerni <hello@michaelaerni.ch>
2020-05-03 13:05:08 +0200

    Add translator modifier

---
+++ Translator/TranslatorModifier.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Statamic\Addons\Translator;
+
+use Statamic\Addons\Translator\Contracts\TranslationService;
+use Statamic\Extend\Modifier;
+
+class TranslatorModifier extends Modifier
+{
+    private $service;
+
+    public function __construct(TranslationService $service)
+    {
+        $this->service = $service;    
+    }
+
+    /**
+     * Modify a value
+     *
+     * @param mixed  $value    The value to be modified
+     * @param array  $params   Any parameters used in the modifier
+     * @param array  $context  Contextual values
+     * @return mixed
+     */
+    public function index($value, $params, $context)
+    {
+        if (!array_get($params, 0)) {
+            return $value;
+        }
+
+        $targetLocale = array_get($params, 0);
+
+        return $this->service->translateText($value, $targetLocale);
+    }
+}
