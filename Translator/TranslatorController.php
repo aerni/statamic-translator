674be461ca36828eb5ca1fbec0d1442802c30091
Michael Aerni <hello@michaelaerni.ch>
2020-05-03 10:30:32 +0200

    Remove unnecessary testing methods

--- Translator/TranslatorController.php
+++ Translator/TranslatorController.php
@@ -4,19 +4,16 @@ namespace Statamic\Addons\Translator;
 
 use Illuminate\Http\Request;
 use Statamic\Extend\Controller;
-use Statamic\Addons\Translator\Contracts\TranslationService;
 
 class TranslatorController extends Controller
 {
     protected $translator;
-    protected $service;
 
-    public function __construct(Translator $translator, TranslationService $service)
+    public function __construct(Translator $translator)
     {
         parent::__construct();
  
         $this->translator = $translator;
-        $this->service = $service;
     }
 
     public function postTranslate(Request $request): array
@@ -35,26 +32,5 @@ class TranslatorController extends Controller
         return [
             'message' => 'Translation successful!',
         ];
-    }
-
-    public function getContract()
-    {
-        $translation = $this->service->translateText('<div class="i">I am the king!</div>', 'de');
-
-        dd($translation);
-    }
-
-    public function getLanguages()
-    {
-        $langauges = $this->service->supportedLanguages();
-
-        dd($langauges);
-    }
-
-    public function getLanguage()
-    {
-        $language = $this->service->detectLanguage('I am a cool guy!');
-
-        dd($language);
     }
 }
