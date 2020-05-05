45b9cb4bb2d04a4d75cc3e5a594d662bacd9044a
Michael Aerni <hello@michaelaerni.ch>
2020-05-05 09:53:47 +0200

    Add German fieldtype translation

---
+++ Translator/resources/lang/de/fieldtype.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'error_default_locale' => 'Die Standardsprache kann nicht übersetzt werden',
+    'error_source_locale' => 'Die Standardsprache wird für die Übersetzung nicht unterstützt',
+    'error_target_locale' => 'Die aktuelle Sprache wird für die Übersetzung nicht unterstützt',
+    'error_unavailable' => 'Translator ist nicht verfügbar',
+    'translate' => 'Inhalt übersetzen',
+    'translating_title' => 'Übersetzung in Bearbeitung',
+    'translating_message' => 'Bitte warte, bis die Übersetzung abgeschlossen ist',
+    'reload' => 'Die Seite wird in 3 Sekunden neu geladen',
+    'success' => 'Übersetzung erfolgreich',
+    'error_general' => 'Ein Fehler ist aufgetreten',
+    'error_console' => 'Bitte prüfe die Konsole für weitere Informationen',
+];
---
+++ Translator/resources/lang/de/fieldtypes.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'button_text' => 'Inhalt-übersetzen-Text',
+    'button_text_instruct' => 'Ändere den Text der "Inhalt übersetzen"-Schaltfläche',
+];
