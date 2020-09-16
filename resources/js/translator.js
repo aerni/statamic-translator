0fcf3b13ae7f2d35aa13af49094ff0ccd776ed53
Michael Aerni <hello@michaelaerni.ch>
2020-09-16 16:18:51 +0200

    Wrap components with Statamic.booting function

--- resources/js/translator.js
+++ resources/js/translator.js
@@ -1,5 +1,7 @@
 // import FlashMessage from './components/FlashMessage';
 import TranslatorFieldtype from './components/TranslatorFieldtype';
 
-// Statamic.$components.register('flash-message', FlashMessage);
-Statamic.$components.register('translator-fieldtype', TranslatorFieldtype);
+Statamic.booting(() => {
+    // Statamic.$components.register('flash-message', FlashMessage);
+    Statamic.$components.register('translator-fieldtype', TranslatorFieldtype);
+});
